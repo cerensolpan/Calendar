632539dc02a270eb0123fcc890d2c48b5a7e3e68
cerensolpan <cerensolpan@gmail.com>
2021-05-09 18:38:36 +0300

    Add illustration and fix errors

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -2,18 +2,20 @@
 export default {
   data() {
     return {
-      days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      days: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
       currentYear: new Date().getFullYear(),
       currentMonth: new Date().getMonth(),
       currentDate: new Date().getUTCDate(),
+      timeSVG: require("../assets/time.svg"),
     };
   },
+
   methods: {
     daysInMonth() {
       return new Date(this.currentYear, this.currentMonth + 1, 0).getDate();
     },
     startDay() {
-      return new Date(this.currentYear, this.currentMonth, 1).getDay();
+      return new Date(this.currentYear, this.currentMonth, 0).getDay();
     },
     currentDateClass(num) {
       const calendarFullDate = new Date(
@@ -48,77 +50,89 @@ export default {
       return new Date(
         this.currentYear,
         this.currentMonth
-      ).toLocaleString("default", { month: "long" });
+      ).toLocaleString("en-US", { month: "long" });
     },
   },
 };
 </script>
 
 <template>
-  <div class="page">
-    <h1>Calendar</h1>
-    <section class="year">
-      <h4>{{ currentMonthName }}</h4>
-      <h4>{{ currentYear }}</h4>
-    </section>
-    <section class="week">
-      <p class="weekday" v-for="day in days" :key="day">{{ day }}</p>
-    </section>
-    <section class="days">
-      <p
-        class="text-center"
-        style="width: 14.28%"
-        v-for="num in startDay()"
-        :key="num"
-      ></p>
-      <p
-        class="date-number"
-        v-for="num in daysInMonth()"
-        :key="num"
-        :class="currentDateClass(num)"
-      >
-        {{ num }}
-      </p>
-    </section>
-    <section class="buttons">
-      <button @click="prev">Prev</button>
-      <button @click="next">Next</button>
-    </section>
+  <div
+    class="page d-flex justify-content-between align-items-center text-center"
+    style="background:#DDE1EC;"
+  >
+    <div>
+      <section>
+        <img
+          :src="timeSVG"
+          class="time-svg ml-5 mr-2"
+          style="max-width:250px"
+        />
+      </section>
+    </div>
+    <div class="time mr-5" style="max-width:400px; min-width:350px;">
+      <h2 class="mt-3">Calendar</h2>
+      <section class="year d-flex justify-content-between p-2">
+        <h4>{{ currentMonthName }}</h4>
+        <h4>{{ currentYear }}</h4>
+      </section>
+      <section class="week d-flex flex-row text-center">
+        <p
+          class="weekday"
+          style="width: 14.28%"
+          v-for="(day, index) in days"
+          :key="index"
+        >
+          {{ day }}
+        </p>
+      </section>
+      <section class="days d-flex flex-row text-center flex-wrap">
+        <p
+          class="text-center"
+          style="width: 14.28%"
+          v-for="(start, index) in startDay()"
+          :key="-1 - index"
+        ></p>
+        <p
+          class="date-number"
+          style="width: 14.28%"
+          v-for="(num, index) in daysInMonth()"
+          :key="index"
+          :class="currentDateClass(num)"
+        >
+          {{ num }}
+        </p>
+      </section>
+      <section class="buttons d-flex justify-content-between">
+        <button @click="prev()">Prev</button>
+        <button @click="next()">Next</button>
+      </section>
+    </div>
   </div>
 </template>
 
 <style>
 .page {
-  margin: auto;
-  display: flex;
-  max-width: 400px;
-  flex-direction: column;
-  text-align: center;
-}
-.year {
-  display: flex;
-  justify-content: space-between;
-  padding: 15px;
-}
-.week {
-  display: flex;
   flex-direction: row;
-  text-align: center;
+  height: 100vh;
+  padding: 0px 75px 0px 75px;
 }
-.weekday {
-  width: 14.28%;
+@media only screen and (max-width: 767px) {
+  .page {
+    flex-direction: column !important;
+  }
+  .time-svg {
+    display: none;
+  }
 }
-.days {
-  display: flex;
-  flex-wrap: wrap;
-  text-align: center;
-  flex-direction: row;
+button {
+  background: #536dfe;
+  padding: 3px 15px 3px 15px;
+  border: 1px solid #dde1ec;
+  border-radius: 8px !important;
+  color: #fff;
 }
-.date-number {
-  width: 14.28%;
-}
-.buttons {
-  display: flex;
-  justify-content: space-between;
+button:hover {
+  background: #7086ff;
 }
 </style>
